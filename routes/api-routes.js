ae769834821b9772eb3f5f1c53441849ce52878e
Ross McLane <ross.mclane@gmail.com>
2019-12-02 14:25:56 -0500

    Seeding Database, Adding Models, Adding API Routes

---
+++ routes/api-routes.js
@@ -0,0 +1,18 @@
+var path = require("path");
+
+module.exports = function(app) {
+  // Post Participants
+  app.post("/participants", function(req, res) {
+    // Code
+  });
+
+  // Get Players
+  app.get("/golfers", function(req, res) {
+    // Code
+  });
+
+  // Get Earnings
+  app.get("/earnings", function(req, res) {
+    // Code
+  });
+};
