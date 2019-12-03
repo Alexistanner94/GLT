becf4d8f82a1f04e66b42364eb3548415c307f7c
Ross McLane <ross.mclane@gmail.com>
2019-12-03 10:14:22 -0500

    Adding id column to Players

--- models/players.js
+++ models/players.js
@@ -1,10 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
   var Players = sequelize.define("Players", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING
   });
