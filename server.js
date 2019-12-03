588f54749b7644e0157a7867c28e5d6c2d4f65f1
Ross McLane <ross.mclane@gmail.com>
2019-12-03 12:17:24 -0500

    Adding Dotenv functionality and Seeding Tournament Database

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
 var express = require("express");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 var db = require("./models");
-// require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
