1e8b434d7caa42835b25b9833e5f9d780665dee0
Alexis Tanner <alexistanner94@hotmail.com>
2019-12-04 11:05:53 -0500

    adding dropbox etc

--- server.js
+++ server.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-const http = require("http");
-PORT = process.env.PORT || 8080;
-server = http.createServer(function(req, res) {
-  response.end("Yay! It Worked!");
-});
-
-server.listen(3000, function() {
-  console.log("Server listening on: http//localhost:" + PORT);
-});
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-=======
 require("dotenv").config();
 var express = require("express");
 
@@ -47,4 +19,3 @@ db.sequelize.sync().then(function() {
     console.log("App listening on PORT " + PORT);
   });
 });
->>>>>>> d096cbab8416fd15cd3f64e688faef8451bda11d
