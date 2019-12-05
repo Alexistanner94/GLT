ee98064b113ceeb742455abec0f5d54d70816aa8
Jonathan Johnson <54649435+jraf7@users.noreply.github.com>
2019-12-05 14:01:25 -0500

    remove duplicates

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,24 +1,10 @@
 var path = require("path");
 
 module.exports = function(app) {
-<<<<<<< HEAD
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-
-    app.get("/buildteam", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/buildteam.html"));
-    });
-
-    app.get("/leaderboard", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/leaderboard.html"));
-    });
 
     app.get("/admin", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/admin.html"));
     });
-};
-=======
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
@@ -30,11 +16,6 @@ module.exports = function(app) {
   app.get("/leaderboard", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/leaderboard.html"));
   });
-
-  app.get("/partadmin", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/partadmin.html"));
-  });
-
   app.get("/about", function(req, res){
     res.sendFile(path.join(__dirname, "../public/about.html"));
   });
@@ -43,4 +24,3 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 };
->>>>>>> 1f58396c179d8d2d447c17f3addb193b1d966385
