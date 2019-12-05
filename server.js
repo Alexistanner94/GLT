f3b1238d27ddba98fb43de556a14bef45da5a508
Jonathan Johnson <54649435+jraf7@users.noreply.github.com>
2019-12-05 14:03:10 -0500

    remove merge boilerplate

--- server.js
+++ server.js
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-const http = require("http");
-PORT = process.env.PORT || 8080;
-server = http.createServer(function(req, res) {
-    response.end("Yay! It Worked!");
-});
 
-server.listen(3000, function() {
-    console.log("Server listening on: http//localhost:" + PORT);
-});
-
-=======
->>>>>>> 1f58396c179d8d2d447c17f3addb193b1d966385
 require("dotenv").config();
 var express = require("express");
 
@@ -28,14 +16,12 @@ require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
 db.sequelize.sync().then(function() {
-<<<<<<< HEAD
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
     });
 });
-=======
+
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
->>>>>>> 1f58396c179d8d2d447c17f3addb193b1d966385
